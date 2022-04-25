71bd2155dae7a19982f7495a57d1b756959ad4ec
KKonaOG <38236274+KKonaOG@users.noreply.github.com>
2022-04-24 22:59:09 -0500

    Create term_emulation.js

---
+++ term_emulation.js
@@ -0,0 +1,29 @@
+// Terminal Input
+var term_input = document.getElementById("term_dialog_input");
+var term_output = document.getElementsByTagName("term")[0];
+term_input.addEventListener("keydown", function(event) {
+    if (event.key == "Enter") {
+        if(term_input.value == "") {
+            return;
+        }
+
+
+
+        commands(term_input.value);
+        term_input.value = "";
+    }
+})
+
+
+
+function commands(cmd) {
+    if (cmd == "help") {
+        term_output.innerHTML = term_output.innerHTML + "<p>Commands: help, projects, clear</p>"
+    } else if (cmd == "projects") {
+        term_output.innerHTML = term_output.innerHTML + "Active Projects: <a href='dissection.html'>Electronics Dissection</a>"
+    } else if (cmd == "clear") {
+        term_output.innerHTML = "";
+    } else {
+        term_output.innerHTML = term_output.innerHTML + "<p style='color:red'> Unknown Command: " + term_input.value + "</p>";
+    }
+}
