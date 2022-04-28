02c1c143adbb1be0e6d2a23b1fedab4db25c59bf
KKonaOG <38236274+KKonaOG@users.noreply.github.com>
2022-04-28 13:27:57 -0500

    Cleanup and color changes

--- assets/js/term_emulation.js
+++ assets/js/term_emulation.js
@@ -1,20 +1,33 @@
 // Terminal Input
 var term_input = document.getElementById("term_dialog_input");
 var term_output = document.getElementsByTagName("term")[0];
-term_input.addEventListener("keydown", function(event) {
+var imgs = document.getElementsByTagName("img");
+for(var k = 0; k < imgs.length; k++){
+    var img = imgs.item(k);
+    img.addEventListener("click", imgClicked)
+}
+
+if (term_input != null) {
+ term_input.addEventListener("keydown", function(event) {
     if (event.key == "Enter") {
         if(term_input.value == "") {
             return;
         }
-
-
-
         commands(term_input.value);
         term_input.value = "";
     }
-})
+})   
+}
 
 
+function imgClicked(event) {
+    let src = event.srcElement;
+    if (src.id == 'selected') {
+        src.id = '';
+    } else {
+        src.id = 'selected';
+    }
+}
 
 function commands(cmd) {
     term_output.innerHTML = term_output.innerHTML + "<p>local_user@kkonaog.github.io > " + cmd + "</p>";
